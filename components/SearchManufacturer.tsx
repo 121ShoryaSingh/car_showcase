885094a6da24818a2dda5ba34bdc577e2d9d5c33
121ShoryaSingh <139441120+121ShoryaSingh@users.noreply.github.com>
2024-08-01 09:12:46 +0530

    Added Transition in combobox options

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,11 +1,15 @@
 "use client"
 
-import React from 'react'
+import React, { useState, Fragment } from 'react'
 import { SearchManufacturerProps } from "@/types"
-import { Combobox, ComboboxButton, Transition } from '@headlessui/react'
+import { Combobox, ComboboxButton, ComboboxInput, Transition } from '@headlessui/react'
 import Image from 'next/image'
 
+
 const SearchManufacturer: React.FC<SearchManufacturerProps> = ({manufacturer , setmanufacturer}) => {
+  
+  const [query, setQuery ] = useState('');
+  
   return (
     <div className=''>
       <Combobox>
@@ -13,6 +17,10 @@ const SearchManufacturer: React.FC<SearchManufacturerProps> = ({manufacturer , s
           <ComboboxButton className="absolute top-[14px]">
             <Image src="/car-logo.svg" alt='Car Logo' width={20} height={20} className='ml-4' />
           </ComboboxButton>
+          <ComboboxInput className="search-manufacturer__input" placeholder='VolksWagen'  displayValue={(manufacturer: string) => manufacturer} onChange={(e) => setQuery(e.target.value)}/>
+          <Transition as={Fragment} leave='Transition ease-in duration-100' leaveFrom='opacity-100' leaveTo='opacity-0' afterLeave={() => setQuery('')} >
+
+          </Transition>
         </div>
       </Combobox>
     </div>
